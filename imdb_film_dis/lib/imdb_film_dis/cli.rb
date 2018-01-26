bd671761ffe3b40cd4d1e3b5d958f9058d1663e0
Raiet Intraub <raietsaeed@gmail.com>
2018-01-26 02:42:06 +0000

    set up intro cli with opening/menu

--- imdb_film_dis/lib/imdb_film_dis/cli.rb
+++ imdb_film_dis/lib/imdb_film_dis/cli.rb
@@ -2,6 +2,29 @@ class ImdbFilmDis::CLI
 
   def call
     puts "Top 10 Celebs of IMDB Sorted by Popularity Ascending"
+    list_top_ten
+    menu
+  end
+
+  def list_top_ten
+    puts "List top ten celebs from IMDB"
+  end
+
+  def menu
+    input = nil
+    while input != "exit"
+      puts "Enter the number of the celeb you would like more info on or select 'exit' or 'list'"
+      input = gets.strip.downcase
+      puts "input #{input}"
+      if input == "list"
+        list_top_ten
+      end
+    end
+    exitProgram
+  end
+
+  def exitProgram
+    puts "Thanks for using this gem!"
   end
 
 end
