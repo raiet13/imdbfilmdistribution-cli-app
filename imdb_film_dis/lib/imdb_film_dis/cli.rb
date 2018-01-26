412f2b488e58743d35ef824c5468d5672e971126
Raiet Intraub <raietsaeed@gmail.com>
2018-01-26 03:05:18 +0000

    Updated menu with proper celeb selection and options

--- imdb_film_dis/lib/imdb_film_dis/cli.rb
+++ imdb_film_dis/lib/imdb_film_dis/cli.rb
@@ -1,3 +1,6 @@
+
+require_relative "./celeb"
+
 class ImdbFilmDis::CLI
 
   def call
@@ -8,19 +11,23 @@ class ImdbFilmDis::CLI
 
   def list_top_ten
     puts "List top ten celebs from IMDB"
-    #@celebs = ImdbFilmDis::Celeb.topten
+    @celebs = ImdbFilmDis::Celeb.topten
   end
 
   def menu
     input = nil
     while input != "exit"
-      puts "Enter the number of the celeb you would like more info on or select 'exit' or 'list'"
+      puts "Enter the number of the celeb you would like more info on, 'exit', or 'list'"
       input = gets.strip.downcase
-      puts "input #{input}"
+      #puts "input #{input}"
       if input == "list"
         list_top_ten
+      elsif input.to_i > 0
+        puts "show celeb #{input.to_i}"
+        @celebs[input.to_i-1]
+      else
+        puts "Please only type in one of the approved actions"
       end
-
     end
     exitProgram
   end
