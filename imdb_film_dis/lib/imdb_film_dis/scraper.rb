8239841031310b0389330105eb84034fdcf92181
Raiet Intraub <raietsaeed@gmail.com>
2018-01-26 22:03:06 +0000

    Scraper creating base Celeb objects

--- imdb_film_dis/lib/imdb_film_dis/scraper.rb
+++ imdb_film_dis/lib/imdb_film_dis/scraper.rb
@@ -9,16 +9,22 @@ class ImdbFilmDis::Scraper
     doc.css("div.lister-list div.lister-item-content").each do |celeb|
       # data = celeb.css("h3.lister-item-header")
       # puts "data = #{data}"
-      rank = celeb.css("h3.lister-item-header span").text.strip
+      rank = celeb.css("h3.lister-item-header span").text.gsub(".", "").strip
       name = celeb.css("h3.lister-item-header a").text.strip
       url = celeb.css("h3.lister-item-header a").attr("href").text.strip
-      puts "name = #{name} || rank = #{rank} || url = #{url}"
+
+      #puts "name = #{name} || rank = #{rank} || url = #{url}"
+      celeb = ImdbFilmDis::Celeb.new(rank, name, url)
     end
+
+    # ImdbFilmDis::Celeb.all.each do |celeb|
+    #   puts "Celeb : #{celeb.rank}. #{celeb.name} - #{celeb.url} - #{celeb.films.size}"
+    # end
   end
 
   def self.scrape_celeb_page(url)
     puts "Scrape Celeb Page : #{url}"
-    doc = Nokogiri::HTML(open(url))
+    doc = Nokogiri::HTML(open("http://www.imdb.com/" + url))
 
   end
 
